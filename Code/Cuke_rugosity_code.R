81669394aee4d1626cd45506baa35050c41bdc2d
Em Lim <emily.lim13@gmail.com>
2024-05-27 18:44:51 -0700

    swap modelling package

--- Code/Cuke_rugosity_code.R
+++ Code/Cuke_rugosity_code.R
@@ -10,11 +10,10 @@ library(dplyr)
 library(stringr)
 library(ggplot2) # Graphing package
 library(visreg)
-# Mixed effects linear models
-source("Code/themes_code.R")
+library(glmmTMB) # swiss army knife of modelling
 
 # Load data ----
-cukes <- read_csv("Data/2022_06_04_cuke_rugosity.csv") %>%
+cukes <- read_csv("Data/2024_05_27_cuke_rugosity.csv") %>%
   rowwise() %>%
   mutate(mean_r = mean(c(r1_m, r2_m)),
          rugosity = 1 - (mean_r/3)
@@ -35,8 +34,7 @@ ggplot(data = cukes, aes(rugosity, cukes)) +
                         low="#5EB7F8", high="#1A334B")
 
 
-ggsave("Output/Cuke_rugosity.png", device = "png",
-       height = 9, width = 16, dpi = 400)
+ggsave("Output/Cuke_rugosity.png", device = "png", height = 9, width = 16, dpi = 400)
 
 # Stats -----
 
