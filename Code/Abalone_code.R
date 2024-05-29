cb8c86a6872b8b2a7fe68fe0ef19cdcff4b56dff
Em Lim <emily.lim13@gmail.com>
2024-05-29 15:07:59 -0700

    try to map. fail

--- Code/Abalone_code.R
+++ Code/Abalone_code.R
@@ -25,11 +25,18 @@ library(patchwork) # Arrange multiple plots together
 library(visreg) # plot model predictions
 library(viridisLite) # colour palettes
 
+library(ggspatial)
+library(sf)
+
 # source file where my functions live
 source("Code/Functions.R")
 
 # Load abalone data 
-abalone <- read_csv("Data/abalone_RLS.csv")
+abalone <- read_csv("Data/abalone_RLS.csv") %>%
+  mutate(site_code = ifelse(site_code == "BMSC28", "BMSC24", site_code),
+         latitude = ifelse(site_code == "BMSC24", "48.91607", latitude),
+         longitude = ifelse(site_code == "BMSC24", "-125.1312", longitude))
+
 
 # Where are abalone? -----
 
@@ -41,6 +48,51 @@ aba_coords <- abalone %>%
          ) %>%
   ungroup() %>%
   select(site_code, site_name, survey_type, latitude, longitude, mean_total, mean_density) %>%
-  unique()
+  unique() 
+  
 
 write_csv(aba_coords, "Data/abalone_coordinates.csv")
+
+
+# Mapping -----
+# load map
+
+a_rank <- aba_coords %>%
+  filter(site_name != "Baeria Rocks North Island Southside") %>%
+  filter(site_name != "Eussen Rock") %>%
+  filter(site_name != "Effingham Archipelago") %>%
+  filter(site_name != "Baeria Rocks North Island Northside") %>%
+  filter(site_name != "Faber Islets") %>%
+  filter(site_name != "Baeria Rocks South Island") %>%
+  filter(site_name != "Effingham West") %>%
+  arrange(desc(mean_density)) %>%
+  head(12)
+  
+# one row for each abalone
+a <- abalone %>%
+  uncount(transect_total)
+  
+most_aba <- a %>%
+  filter(site_code %in% a_rank$site_code) 
+
+idk <- abalone %>%
+  group_by(site_name, depth) %>% 
+  summarise(mean_den = mean(density))
+
+
+hist(a$depth)
+
+# do we want to look at the sites?
+# maybe not bc this is more of a hist of where we did transects...
+most_aba %>%
+  ggplot(aes(x=depth, color=site_name, fill=site_name)) +
+  geom_histogram(alpha=0.5, binwidth = 0.5, position = 'identity') +
+#  scale_fill_viridis(discrete=TRUE) +
+#  scale_color_viridis(discrete=TRUE) +
+  theme_classic() +
+  theme(
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8)
+  ) +
+  xlab("") +
+  ylab("Assigned Probability (%)")
